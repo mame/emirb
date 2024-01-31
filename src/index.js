50fc0c68dbbe2a6aa9883809b0b8cadda116ec30
Yusuke Endoh <mame@ruby-lang.org>
2024-01-31 20:40:37 +0900

    Use xterm-pty 0.10.1 and cosmetic change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import { loadRuby } from "ruby-wasm-emscripten";
-import { Terminal } from "xterm";
-import { FitAddon } from "xterm-addon-fit";
-import { openpty } from "xterm-pty";
+import { Terminal } from "@xterm/xterm";
+import { FitAddon } from "@xterm/addon-fit";
+import { WebLinksAddon } from "@xterm/addon-web-links";
+import { openpty, Flags } from "xterm-pty";
 
 var div = document.getElementById("terminal");
 
@@ -11,13 +12,42 @@ xterm.open(div);
 const { master, slave } = openpty();
 xterm.loadAddon(master);
 
+const fitAddon = new FitAddon();
+xterm.loadAddon(fitAddon);
+new ResizeObserver(() => fitAddon.fit()).observe(div);
+fitAddon.fit();
+
+xterm.loadAddon(new WebLinksAddon());
+
+const termios = slave.ioctl("TCGETS");
+slave.ioctl("TCSETS", { ...termios, lflag: termios.lflag & ~0x0008 });
+
+slave.write("  \x1b[1;36m      "  + "            "  + " \x1b[1;31m _\x1b[m\n");
+slave.write("  \x1b[1;36m      "  + "            "  + " \x1b[1;31m(_)        .-.\x1b[m\n");
+slave.write("  \x1b[1;36m  ___ "  + "  _ __ ___  "  + " \x1b[1;31m _   _ __  | |__\x1b[m\n");
+slave.write("  \x1b[1;36m / _ \\" + " | '_ ` _ \\ " + " \x1b[1;31m| | | '_ | | '_ \\\x1b[m\n");
+slave.write("  \x1b[1;36m|  __/"  + " | | | | | |"  + " \x1b[1;31m| | | | '' | |_) |\x1b[m\n");
+slave.write("  \x1b[1;36m \\___|" + " |_| |_| |_|"  + " \x1b[1;31m|_| |_|    |_.__/\x1b[m\n");
+slave.write("\n");
+slave.write("* source code: https://github.com/mame/emirb\n");
+slave.write("* powered by\n");
+slave.write("  * ruby-wasm-emscripten: https://github.com/ruby/ruby.wasm\n");
+slave.write("  * xterm-pty: https://xterm-pty.netlify.app/\n");
+slave.write("\n");
+
+
 const main = async () => {
   const args = ["--disable-gems", "-e", "require 'irb'; IRB.setup(nil, argv: ['--no-pager']); IRB::Irb.new.run"];
   console.log(`$ ruby.wasm ${args.join(" ")}`);
   const defaultModule = {
     locateFile: (path) => "./ruby-wasm-emscripten-dist/" + path,
     setStatus: (msg) => {
-      document.getElementById("status").innerText = msg == "" ? "Ready" : msg;
+      if (msg == "") {
+        slave.ioctl("TCSETS", { ...termios });
+      }
+      else {
+        slave.write("\r\x1b[0K" + msg);
+      }
     },
     print: (line) => {
       if (document) {
