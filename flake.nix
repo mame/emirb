92722f5c1ddc5d807c2539daa86a15e721072394
elcuervo <elcuervo@elcuervo.net>
2024-02-08 16:51:15 -0300

    Adds flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            ruby_3_2
+            gnumake
+            nodePackages_latest.wrangler
+          ];
+        };
+      });
+}
